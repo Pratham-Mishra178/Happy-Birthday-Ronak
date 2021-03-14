580a34a1ca1c65694057639f674e3c7a1642980c
Pratham Mishra <prathammishra178@gmail.com>
2021-03-14 15:14:53 +0530

    Terminal

--- Birthday_Terminal_Version.c
+++ Birthday_Terminal_Version.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <time.h>
+
 void delay(int number_of_seconds)
 {
-    int milli_seconds = 250 * number_of_seconds;
+    int milli_seconds = 100 * number_of_seconds;
     clock_t start_time = clock();
     while (clock() < start_time + milli_seconds)
         ;
@@ -11,69 +12,262 @@ void delay(int number_of_seconds)
 int main()
 {
     {
+        delay(1);
         printf("\n        __                                                                                                                __  ");
+        delay(1);
         printf("\n       / /  __  __                           ____  _      __  __        __               ____                    __      / /  ");
+        delay(1);
         printf("\n      / /  / / / /___ _____  ____  __  __   / __ )(_)____/ /_/ /_  ____/ /___ ___  __   / __ |____  ____  ____ _/ /__   / /   ");
+        delay(1);
         printf("\n     / /  / /_/ / __ `/ __ |/ __ |/ / / /  / __  / / ___/ __/ __ |/ __  / __ `/ / / /  / /_/ / __ |/ __ |/ __ `/ //_/  / /    ");
+        delay(1);
         printf("\n    / /  / __  / /_/ / /_/ / /_/ / /_/ /  / /_/ / / /  / /_/ / / / /_/ / /_/ / /_/ /  / _, _/ /_/ / / / / /_/ / ,<    / /     ");
+        delay(1);
         printf("\n   / /  /_/ /_/|__,_/ .___/ .___/|__, /  /_____/_/_/   |__/_/ /_/|__,_/|__,_/|__, /  /_/ |_||____/_/ /_/|__,_/_/|_|  / /      ");
+        delay(1);
         printf("\n  /_/              /_/   /_/    /____/                                      /____/                                  /_/       ");
     }
+    printf("\n\n");
     {
-        delay(8);
+        delay(1);
+        printf("\n                              .--------.                        ,,,,,,,,,,,,,");
+        delay(1);
+        printf("\n                             .: : :  :___`.                 .;;;;;;;;;;;;;;;;;;;,.");
+        delay(1);
+        printf("\n                           .'!!:::::  \\_| `.              .;;;;;;;;;;;;;;;;;;;;;;;;,");
+        delay(1);
+        printf("\n                      : . /%O!!::::::::\\_|.  |           .;;;;;;;;;;;;;;;;;;;;;;;;;;;;.");
+        delay(1);
+        printf("\n                     ['']/%%O!!:::::::::  : . |          ;;;;;@;;;;;;;;;;;;;;;;;;;;;;;;' .............");
+        delay(1);
+        printf("\n                     |  |%%OO!!::::::::::: : . |         ;;;;@@;;;;;;;;;;;;;;;;;;;;;;;;'.................");
+        delay(1);
+        printf("\n                     |  |%%OO!!:::::::::::::  :|         ;;;;@@;;;;;;;;;;;;;;;;;;;;;;;;'...................");
+        delay(1);
+        printf("\n                     |  |%%OO!!!::::::::::::: :|         `;;;;@;;;;;;;;;;;;;;;@;;;;;;;'.....................");
+        delay(1);
+        printf("\n            :       .'--`.%%OO!!!:::::::::::: :|          `;;;;;;;;;;;;;;;;;;;@@;;;;;'..................;....");
+        delay(1);
+        printf("\n          : .:     /`.__.'|%%OO!!!::::::::::::/             `;;;;;;;;;;;;;;;;@@;;;;'....................;;...");
+        delay(1);
+        printf("\n         :    .   /        |%OO!!!!::::::::::/                `;;;;;;;;;;;;;@;;;;'...;.................;;....");
+        delay(1);
+        printf("\n        ,-'``'-. ;          ;%%OO!!!!!!:::::'                    `;;;;;;;;;;;;'   ...;;...............;.....");
+        delay(1);
+        printf("\n        |`-..-'| |   ,--.   |`%%%OO!!!!!!:'                          `;;;;;;'        ...;;..................");
+        delay(1);
+        printf("\n        | .   :| |_.','`.`._|  `%%%OO!%%'                                ;;              ..;...............");
+        delay(1);
+        printf("\n        | . :  | |--'    `--|    `%%%%'                                 `                  ............");
+        delay(1);
+        printf("\n        |`-..-'| ||   | | | |     /__|`-.                            `                      ......");
+        delay(1);
+        printf("\n        |::::::/ ||)|/|)|)|||           /                           `                         ..");
+        delay(1);
+        printf("\n---------`::::'--|._ ~**~ _.|----------( -----------------------   `                           '");
+        delay(1);
+        printf("\n           )(    |  `-..-'  |           |    ______               `                           '");
+        delay(1);
+        printf("\n           )(    |          |,--.       ____/ /  /|| ,-._.-'     `                           '");
+        delay(1);
+        printf("\n        ,-')('-. |          ||`;/   .-()___  :  |`.!,-'`'/`-._  `                           `");
+        delay(1);
+        printf("\n       (  '  `  )`-._    _.-'|;,|    `-,    |_|__|`,-'>-.,-._   `                           `,");
+        delay(1);
+        printf("\n        `-....-'     ````    `--'      `-._       (`- `-._`-.   `");
+        delay(1);
+        printf("\n                                                                 `");
+        delay(1);
+        printf("\n                                                                   `.");
+    }
+    {
+        delay(1);
         printf("\n\n\n\n                                                       * ");
+        delay(1);
         printf("\n                  *                                             * ");
+        delay(1);
         printf("\n                            * ");
+        delay(1);
         printf("\n                                          * ");
+        delay(1);
         printf("\n                                                                    * ");
-        delay(6);
+        delay(1);
         printf("\n                 * ");
+        delay(1);
         printf("\n                                                          * ");
+        delay(1);
         printf("\n                     * ");
+        delay(1);
         printf("\n                                   *             * ");
+        delay(1);
         printf("\n                                                             * ");
+        delay(1);
         printf("\n              *                                                               * ");
-        delay(6);
+        delay(1);
         printf("\n                       * ");
+        delay(1);
+        delay(1);
         printf("\n                                       (             ) ");
+        delay(1);
         printf("\n                               )      (*)           (*)      ( ");
-        printf("\n                      *       (*)      |             |      (*) ");
-        printf("\n                               |      |~|           |~|      |          * ");
+        delay(1);
+        printf("\n                      ");
+        printf("*");
+        printf("       (*)");
+        delay(1);
+        printf("      |             |      ");
+        delay(1);
+        printf("(*) ");
+        delay(1);
+        printf("\n                               |      |~|           |~|      |          ");
+        printf("* ");
+        delay(1);
         printf("\n                              |~|     | |           | |     |~| ");
+        delay(1);
         printf("\n                              | |     | |           | |     | | ");
-        delay(6);
-        printf("\n                             ,| |a@@@@| |@@@@@@@@@@@| |@@@@a| |. ");
-        printf("\n                        .,a@@@| |@@@@@| |@@@@@@@@@@@| |@@@@@| |@@@@a,. ");
-        printf("\n                      ,a@@@@@@| |@@@@@@@@@@@@.@@@@@@@@@@@@@@| |@@@@@@@a, ");
+        delay(1);
+        printf("\n                             ,");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("a@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@@@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@a");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf(". ");
+        delay(1);
+        printf("\n                        .,a@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@@@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@a,. ");
+        delay(1);
+        printf("\n                      ,a@@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@@@@@@@@.@@@@@@@@@@@@@@");
+        delay(1);
+        printf("| |");
+        delay(1);
+        printf("@@@@@@@a, ");
+        delay(1);
         printf("\n                     a@@@@@@@@@@@@@@@@@@@@@' . `@@@@@@@@@@@@@@@@@@@@@@@@a ");
+        delay(1);
         printf("\n                     ;`@@@@@@@@@@@@@@@@@@'   .   `@@@@@@@@@@@@@@@@@@@@@'; ");
+        delay(1);
         printf("\n                     ;@@@`@@@@@@@@@@@@@'     .     `@@@@@@@@@@@@@@@@'@@@; ");
+        delay(1);
         printf("\n                     ;@@@;,.aaaaaaaaaa       .       aaaaa,,aaaaaaa,;@@@; ");
-        delay(6);
+        delay(1);
         printf("\n                     ;;@;;;;@@@@@@@@;@      @.@      ;@@@;;;@@@@@@;;;;@@; ");
+        delay(1);
         printf("\n                     ;;;;;;;@@@@;@@;;@    @@ . @@    ;;@;;;;@@;@@@;;;;;;; ");
+        delay(1);
         printf("\n                     ;;;;;;;;@@;;;;;;;  @@   .   @@  ;;;;;;;;;;;@@;;;;@;; ");
+        delay(1);
         printf("\n                     ;;;;;;;;;;;;;;;;;@@     .     @@;;;;;;;;;;;;;;;;@@@; ");
-        printf("\n                 ,^^^;;;;;;;;@;;;;;;;;       .       ;;;;;;;;;;;;;;;;@@;;^^^, ");
-        printf("\n              .^^^^^^;;;;;;;@@;;;;;;;;     ,^^^,     ;;;;;;;;;;;;;;;;;;;;^^^^^^, ");
-        printf("\n             .^^^^^^^;;;;;;;@@;;;;;;;;   ,^^^^^^^,   ;;;;;;;;;;;;;;;;;;;;^^^^^^^, ");
-        delay(6);
-        printf("\n             ^^^^^^^^`;;;;;;;;;;;;;;;;  ^^^^^^^^^^^  ;;;;;;;;;;;;;;;;;;;'^^^^^^^^ ");
-        printf("\n             ^^^^^^^^^^^^`;;;;;;;;;;;;,^^^^^^^^^^^^^,;;;;;;;;;;;;;;;'^^^^^^^^^^^^ ");
-        printf("\n              ^^^^^^^^^^^^^^^^^,,,,,,,^^^^^^^^^^^^^^^,,,,,,,^^^^^^^^^^^^^^^^^^^^  ");
+        delay(1);
+        printf("\n                 ,^^^");
+        delay(1);
+        printf(";;;;;;;;@;;;;;;;;");
+        delay(1);
+        printf("       .       ");
+        delay(1);
+        printf(";;;;;;;;;;;;;;;;@@;;");
+        delay(1);
+        printf("^^^, ");
+        delay(1);
+        printf("\n              .^^^^^^");
+        delay(1);
+        printf(";;;;;;;@@;;;;;;;;");
+        delay(1);
+        printf("     ,^^^,     ");
+        delay(1);
+        printf(";;;;;;;;;;;;;;;;;;;;");
+        delay(1);
+        printf("^^^^^^, ");
+        delay(1);
+        printf("\n             .^^^^^^^");
+        delay(1);
+        printf(";;;;;;;@@;;;;;;;;");
+        delay(1);
+        printf("   ,^^^^^^^,   ");
+        delay(1);
+        printf(";;;;;;;;;;;;;;;;;;;;");
+        delay(1);
+        printf("^^^^^^^, ");
+        delay(1);
+        printf("\n             ^^^^^^^^");
+        delay(1);
+        printf("`;;;;;;;;;;;;;;;;");
+        delay(1);
+        printf("  ^^^^^^^^^^^  ");
+        delay(1);
+        printf(";;;;;;;;;;;;;;;;;;;'");
+        delay(1);
+        printf("^^^^^^^^ ");
+        delay(1);
+        printf("\n             ^^^^^^^^^^^^");
+        delay(1);
+        printf("`;;;;;;;;;;;;,");
+        delay(1);
+        printf("^^^^^^^^^^^^^");
+        delay(1);
+        printf(",;;;;;;;;;;;;;;;'");
+        delay(1);
+        printf("^^^^^^^^^^^^ ");
+        delay(1);
+        printf("\n              ^^^^^^^^^^^^^^^^^");
+        delay(1);
+        printf(",,,,,,,");
+        delay(1);
+        printf("^^^^^^^^^^^^^^^");
+        delay(1);
+        printf(",,,,,,,");
+        delay(1);
+        printf("^^^^^^^^^^^^^^^^^^^^  ");
+        delay(1);
         printf("\n                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ");
+        delay(1);
         printf("\n                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ");
+        delay(1);
         printf("\n                          ^^^^^^^^^^^^^^`,,,,,,,,, ^^^^^^^^^^^^^^^^^ ");
-        delay(6);
+        delay(1);
         printf("\n                                         `^^^^^^^` ");
+        delay(1);
         printf("\n                                          `^^^^^` ");
+        delay(1);
         printf("\n                                            ^^^      ");
+        delay(1);
         printf("\n                                           ^^^^^ ");
+        delay(1);
         printf("\n                                        .,^^^^^^^,. ");
+        delay(1);
         printf("\n                                   ,^^^^^^^^^^^^^^^^^^^, ");
     }
 
-    delay(5);
+    delay(1);
     {
         while ("&free")
         {
@@ -81,48 +275,44 @@ int main()
             for (int i = 30; i <= 36; i++)
             {
                 printf("\n");
-                delay(1);
+                delay(2);
                 for (int j = 1; j <= i; j++)
                 {
                     printf(" ");
                 }
-
                 printf("%c  Happy Birthday Ronak Bhai  %c", c, c);
             }
 
             for (int i = 30; i <= 36; i++)
             {
                 printf("\n");
-                delay(1);
+                delay(2);
                 for (int j = -6; j <= i; j++)
                 {
                     printf(" ");
                 }
-
                 printf("%c  Happy Birthday Ronak Bhai  %c", c, c);
             }
 
             for (int i = 37; i >= 30; i--)
             {
                 printf("\n");
-                delay(1);
+                delay(2);
                 for (int j = -6; j <= i; j++)
                 {
                     printf(" ");
                 }
-
                 printf("%c  Happy Birthday Ronak Bhai  %c", c, c);
             }
 
             for (int i = 37; i >= 30; i--)
             {
                 printf("\n");
-                delay(1);
+                delay(2);
                 for (int j = 2; j <= i; j++)
                 {
                     printf(" ");
                 }
-
                 printf("%c  Happy Birthday Ronak Bhai  %c", c, c);
             }
         }
